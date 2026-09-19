c477f380b59f1916c71b4b6c9086c04f209190c2
yl <119298601+hypersimplex@users.noreply.github.com>
2026-09-19 14:02:47 -0700

    doc: pipeline: add todo comments about pattern compilation, join point

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -232,6 +232,12 @@ pub(crate) fn compile(content: &str) -> CompileResult {
         }
     }
 
+    // [todo] compile pattern matching using decision tree for generating more
+    // efficient code
+
+    // [todo, optional] reduce code size by introducing join points; reference:
+    // Compiling without continuations (2017)
+
     println!("normalize constructor wildcards to fresh variable binders..");
     for group in ty_check_results.iter_mut() {
         for (id, top_lvl_fn) in group.iter_mut() {
